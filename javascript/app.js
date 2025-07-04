0c90c2073caec326c55922cbcc0a5f96e24c108d
Devin <devinm3333@outlook.com>
2025-07-04 04:46:23 -0700

    add section 06 and 02-markdown-join-test.md

---
+++ javascript/app.js
@@ -0,0 +1,8 @@
+// Start by defining your function
+function writeConsoleLog(){
+  console.log( 'write to console' )
+}
+
+writeConsoleLog() // Call the function
+writeConsoleLog() // Call the function again
+writeConsoleLog() // example of re-usability
